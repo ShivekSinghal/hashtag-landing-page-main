8863ee21d0e026d13710da0ec7ac852c9e9710e0
ShivekSinghal <singhalshivek24@gmail.com>
2024-04-16 13:47:37 +0530

    Final Commit in respect to Pink'd

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"]= 'filesystem'
 
 
+
 # Google Sheets API credentials
 scopes = ['https://www.googleapis.com/auth/spreadsheets']
 
